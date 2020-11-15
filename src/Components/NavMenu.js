51fa0b660f39c90e515c5d948bab37ad5e9847ed
Frozies <52686922+Frozies@users.noreply.github.com>
2020-11-14 23:00:45 -0500

    Css stuff with navbar

---
+++ src/Components/NavMenu.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {ReactComponent as Menu} from "../images/icons8-xbox-menu.svg";
+import {useTransition, animated} from 'react-spring'
+
+function NavMenu() {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const menuTransitions = useTransition(isMenuOpen, null, {
+        from: { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-100%)' },
+    })
+
+    return (
+        <div className={"nav-menu"}>
+            {
+                menuTransitions.map(({ item, key, props }) =>
+                    item &&
+                    <animated.div
+                        key={key}
+                        style={props}
+                        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+                    >
+                        {/*<NavigationMenu*/}
+                        {/*    closeMenu={() => setShowMenu(false)}*/}
+                        {/*/>*/}
+                        <div className={"navMenu"}>
+                            World
+                        </div>
+                    </animated.div>
+                )
+            }
+
+            <button className={"nav-button"} type={"button"} onClick={()=>setMenuOpen(!isMenuOpen)}>
+                <Menu/>
+            </button>
+            <h1 className={"nav-text"}>
+                Petal Patch
+            </h1>
+
+        </div>
+    );
+}
+export default NavMenu;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,14 @@
 import React from "react";
 import {ReactComponent as MagnifyIcon} from '../images/icons8-search.svg';
-import {ReactComponent as ShoppingCart} from '../images/icons8-food-cart.svg';
-import {ReactComponent as Menu} from '../images/icons8-xbox-menu.svg';
+import NavMenu from "./NavMenu";
+import NavCart from "./NavCart";
 
 
 function Navbar(){
     return (
        <header className={"nav"}>
 
-           <div className={"nav-menu"}>
-               <button className={"nav-button"} type={"button"}>
-                   <Menu/>
-               </button>
-               <h1 className={"nav-text"}>
-                   Petal Patch
-               </h1>
-           </div>
+           <NavMenu/>
 
            <div className={"nav-search"}>
                <input className={"nav-search-bar"} placeholder="What are you looking for?" type="text"/>
@@ -24,14 +17,8 @@ function Navbar(){
                </button>
            </div>
 
-           <div className={"nav-cart"}>
-               <button className={"nav-button"} type={"button"}>
-                   <ShoppingCart/>
-                   <div className={"nav-cart-amount"}>
-                       10
-                   </div>
-               </button>
-           </div>
+           <NavCart/>
+
        </header>
     )
 }
