dfabe5b726ce46097b62bde1487a904c14209ffb
Frozies <52686922+Frozies@users.noreply.github.com>
2020-12-02 21:02:14 -0500

    Icons and footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,27 @@
 import React from "react";
+import {ReactComponent as Mana} from "../../images/Icons/icons8-mana-64.svg";
 
 
 function Footer(){
     return (
         <div  className={"footer"}>
             <div className={"footer"}>
+                {/*Left*/}
+                {/*Petal Patch*/}
+                {/*Address*/}
+                {/*Phone Number*/}
+
+                {/*Centered*/}
                 <p className={"footer-text"}>Copyright &copy; {(new Date().getFullYear())}</p>
+
+                {/*Put this below the copyright*/}
+                <div>
+                    <Mana/>
+                    <h3>Made with love by Davin</h3>
+                    <Mana/>
+                </div>
+
+                {/*Social Media Icons*/}
             </div>
         </div>
     )
