9199781ac1ce25f35eff3605a02b192364382add
Frozies <52686922+Frozies@users.noreply.github.com>
2020-12-03 20:32:29 -0500

    Did some layout stuff on the homepage. adjusted about layout. Adjusted award layout. adjusted footer layout

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,28 +1,40 @@
 import React from "react";
 import {ReactComponent as Mana} from "../../images/Icons/icons8-mana-64.svg";
+import {ReactComponent as Facebook} from "../../images/Icons/icons8-facebook-64.svg";
+import {ReactComponent as Phone} from "../../images/Icons/icons8-phone-64.svg";
+import {ReactComponent as Instagram} from "../../images/Icons/icons8-instagram-64.svg";
+import {ReactComponent as Yelp} from "../../images/Icons/icons8-yelp-64.svg";
+import {ReactComponent as Maps} from "../../images/Icons/icons8-google-maps-old-64.svg";
 
 
 function Footer(){
     return (
         <div  className={"footer"}>
-            <div className={"footer"}>
-                {/*Left*/}
+            <div className={"footer-address"}>
+                239-xxx-xxxx
                 {/*Petal Patch*/}
                 {/*Address*/}
                 {/*Phone Number*/}
-
-                {/*Centered*/}
-                <p className={"footer-text"}>Copyright &copy; {(new Date().getFullYear())}</p>
-
-                {/*Put this below the copyright*/}
-                <div>
-                    <Mana/>
-                    <h3>Made with love by Davin</h3>
-                    <Mana/>
-                </div>
-
-                {/*Social Media Icons*/}
             </div>
+
+            <div className={"footer-copy"}>
+
+                <h5>Copyright &copy; {(new Date().getFullYear())}</h5>
+                <span>
+                    <Mana/>
+                        <h5>Made with love by Davin</h5>
+                    <Mana/>
+                </span>
+            </div>
+
+            <div className={"footer-social"}>
+                <Facebook/>
+                <Phone/>
+                <Instagram/>
+                <Yelp/>
+                <Maps/>
+            </div>
+
         </div>
     )
 }
--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -6,20 +6,30 @@ import award2020 from "../../images/Awards/2020Award.jpg"
 
 function About(){
     return (
-        <div className={"about"}>
-            <h1>About</h1><b/>
-            <span>
-                We here at The Petal Patch are more than just a florist we are family. We love what we do here
-                everyday and this is our happy place. Our arrangements are made with smiles and love. With more then
-                25yrs of floral design let us design the perfect bouquet for you! Whether it be an anniversary,
-                birthday, get well, event, holiday, thinking of you, new baby, new home, wedding or sympathy
-                arrangement we got you covered! Our staff is polite, super talented and we always deliver with a
-                smile.
-            </span>
-            <img src={Bandit} alt={"Bandit"}/><b/>
-            <img src={award2018} alt={"Bandit"}/>
-            <img src={award2019} alt={"Bandit"}/>
-            <img src={award2020} alt={"Bandit"}/>
+        <div>
+            <div className={"about"}>
+                <h1>About</h1><b/>
+
+                <img src={Bandit} alt={"Bandit"}/><b/>
+
+                <span>
+                    We here at The Petal Patch are more than just a florist we are family. We love what we do here
+                    everyday and this is our happy place. Our arrangements are made with smiles and love. With more then
+                    25yrs of floral design let us design the perfect bouquet for you! Whether it be an anniversary,
+                    birthday, get well, event, holiday, thinking of you, new baby, new home, wedding or sympathy
+                    arrangement we got you covered! Our staff is polite, super talented and we always deliver with a
+                    smile.
+                </span>
+            </div>
+
+
+
+            <div className={"about-awards"}>
+                <img src={award2018} alt={"2018 Award"}/>
+                <img src={award2019} alt={"2019 Award"}/>
+                <img src={award2020} alt={"2020 Award"}/>
+            </div>
+
 
 
         </div>
