cbe5b9e9650774d9d8707adec1621fd9c8da168d
Frozies <52686922+Frozies@users.noreply.github.com>
2020-12-06 23:03:36 -0500

    Fixed a bug with the menu overlay staying inside of a parent div and not properly laying on top of the webpage.
    I fixed this by wrapping the animation function inside of a div with fixed position and 999 z-index. super simple fix.

--- src/components/navigation/NavMenu.js
+++ src/components/navigation/NavMenu.js
@@ -5,41 +5,47 @@ import {useTransition, animated} from 'react-spring'
 function NavMenu() {
     const [isMenuOpen, setMenuOpen] = useState(false);
 
+    //Fade in for menu
     const menuTransitions = useTransition(isMenuOpen, null, {
-        from: { opacity: 0, transform: 'translateX(-100%)' },
+        from: { opacity: 0, transform: 'translateX(-100%)'},
         enter: { opacity: 1, transform: 'translateX(0%)' },
         leave: { opacity: 0, transform: 'translateX(-100%)' },
     })
 
     return (
         <div className={"nav-menu"}>
-            {
-                menuTransitions.map(({ item, key, props }) =>
-                    item &&
-                    <animated.div
-                        key={key}
-                        style={props}
-                        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
-                    >
-                        {/*<NavigationMenu*/}
-                        {/*    closeMenu={() => setShowMenu(false)}*/}
-                        {/*/>*/}
-                        <div className={"navMenu"}>
-                            World
-                        </div>
 
-                    </animated.div>
-                )
-            }
+            <div className={"navMenu-anim"}>
+                {
+                    menuTransitions.map(({ item, key, props }) =>
+                        item &&
+                        <animated.div
+                            key={key}
+                            style={props}
+                        >
+                            {/*<NavigationMenu
+                            https://codesandbox.io/s/9o3lw5792w?file=/src/Dropdown.js:211-412 todo: add clicking outside of box.
+                            */}
 
 
 
+                            <div className={"navMenu"} capture}>
+                                World         dfasdfasdfasdf sdfas dfasdf asdfa sdf
+                            </div>
+
+                        </animated.div>
+                    )
+                }
+            </div>
 
 
+            {/*Actual button*/}
             <button className={"nav-button"} type={"button"} onClick={()=>setMenuOpen(!isMenuOpen)}>
                 <Menu/>
             </button>
-            <h1 className={"nav-text"}>
+
+            {/*Title*/}
+            <h1 className={"nav-title"}>
                 Petal Patch
             </h1>
 
