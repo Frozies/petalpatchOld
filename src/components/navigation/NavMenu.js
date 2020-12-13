78460c3b1832fdbb533a3b3122db1cd36e78f803
Frozies <52686922+Frozies@users.noreply.github.com>
2020-12-12 20:44:24 -0500

    Navbar stuff

--- src/components/navigation/NavMenu.js
+++ src/components/navigation/NavMenu.js
@@ -1,47 +1,25 @@
 import React, { useState } from 'react';
-import {ReactComponent as Menu} from "../../images/Icons/icons8-xbox-menu.svg";
-import {useTransition, animated} from 'react-spring'
+import {ReactComponent as MenuIcon} from "../../images/Icons/icons8-xbox-menu.svg";
+ import { slide as Menu } from 'react-burger-menu'
 
 function NavMenu() {
     const [isMenuOpen, setMenuOpen] = useState(false);
-
-    //Fade in for menu
-    const menuTransitions = useTransition(isMenuOpen, null, {
-        from: { opacity: 0, transform: 'translateX(-100%)'},
-        enter: { opacity: 1, transform: 'translateX(0%)' },
-        leave: { opacity: 0, transform: 'translateX(-100%)' },
-    })
 
     return (
         <div className={"nav-menu"}>
 
             <div className={"navMenu-anim"}>
-                {
-                    menuTransitions.map(({ item, key, props }) =>
-                        item &&
-                        <animated.div
-                            key={key}
-                            style={props}
-                        >
-                            {/*<NavigationMenu
-                            https://codesandbox.io/s/9o3lw5792w?file=/src/Dropdown.js:211-412 todo: add clicking outside of box.
-                            */}
-
-
-
-                            <div className={"navMenu"} capture}>
-                                World         dfasdfasdfasdf sdfas dfasdf asdfa sdf
-                            </div>
-
-                        </animated.div>
-                    )
-                }
+                <Menu isOpen={ isMenuOpen }>
+                    <a id="home" className="menu-item" href="/">Home</a>
+                    <a id="about" className="menu-item" href="/about">About</a>
+                    <a id="contact" className="menu-item" href="/contact">Contact</a>
+                </Menu>
             </div>
 
 
             {/*Actual button*/}
             <button className={"nav-button"} type={"button"} onClick={()=>setMenuOpen(!isMenuOpen)}>
-                <Menu/>
+                <MenuIcon/>
             </button>
 
             {/*Title*/}
