848f9b5b1be4a9b9989e04260c35ce15afb93c46
Frozies <52686922+Frozies@users.noreply.github.com>
2020-12-13 20:21:55 -0500

    Added marker to map

--- src/components/homepage/GoogleMap.js
+++ src/components/homepage/GoogleMap.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { compose, withProps } from "recompose";
 import {
     withScriptjs,
@@ -8,7 +7,11 @@ import {
     Marker
 } from "react-google-maps";
 
+let lat = 26.55775;
+let long = -81.90906;
+
 const MyMapComponent = compose(
+
     withProps({
         googleMapURL:
             "https://maps.googleapis.com/maps/api/js?key=AIzaSyAAJQ-3KmHfoUbuLYmUgzNQ-mZn6S1j7mI&v=3.exp&libraries=geometry,drawing,places",
@@ -19,10 +22,8 @@ const MyMapComponent = compose(
     withScriptjs,
     withGoogleMap
 )(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        {props.isMarkerShown && (
-            <Marker position={{ lat: -34.397, lng: 150.644 }} />
-        )}
+    <GoogleMap defaultZoom={20} defaultCenter={{ lat: lat, lng: long }}>
+        <Marker position={{ lat: lat, lng: long }} />
     </GoogleMap>
 ));
 export default MyMapComponent
