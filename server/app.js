e8efdedf2e3185021871f5bd1fa6dbd3b87408e4
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-09 18:37:35 -0500

    Regenerated the server files

--- server/app.js
+++ server/app.js
@@ -1,64 +1,41 @@
-/*Puppeteer is a google made node library which provides api control to a headless chrome.f*/
-const puppeteer = require('puppeteer');
-
-/*Express is the backend driver for all of the website. its like the traffic lights for the flow of data*/
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 const app = express();
 
-/*Express's use function takes in a request gives a response and then directs the next path, or next function to go to.*/
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    /*Getting the headers and access control from the current http address*/
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next(); /*NEXT :D*/
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/', async (req, res) => {
-    res.write("Hello world!");
-    res.send();
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.get('/admin/', async (req, res) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('https://www.teleflora.com/bouquet/make-a-wish?prodID=P_TEV13-6A&skuId=TEV13-6A&zipMin='); // URL is given by the "user" (your client-side application)
-
-    const PRICE_SELECTOR = '#sizeRadios > li:nth-child(INDEX)';
-
-    res.set('Content-Type', 'text/html');
-    let priceSelector = PRICE_SELECTOR.replace("INDEX", i=1);//TODO Replace the value for i for multiple products
-
-    let productName = await page.evaluate((sel, i) => {
-        return document.getElementById("skuRadio" + i).getAttribute('data-alttext');
-    }, priceSelector, i);
-
-    // Product ID
-    let productID = await page.evaluate((sel, i) => {
-        return document.getElementById("skuRadio" + i).value;
-    }, priceSelector, i);
-
-    // Price of Product
-    let price = await page.evaluate((sel, i) => {
-        return document.getElementById("skuid-" + i).value;
-    }, priceSelector, i);
-
-    // Product Image
-    let productImage = await page.evaluate((sel, i) => {
-        return document.getElementById("skuRadio" + i).getAttribute('data-zoom');
-    }, priceSelector, i);
-
-    res.write(JSON.stringify({
-        name: productName,
-        id: productID,
-        price: price,
-        image: productImage.replace("/w_800,h_1000", "/w_300,h_300"), // Image size
-    }))
-
-    res.send();
-    await browser.close();
-})
-
-app.listen(4000);
+module.exports = app;
