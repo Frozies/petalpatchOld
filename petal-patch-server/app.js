c4a514e235fbddd566720d33c965603f86f1d96a
Frozies <52686922+Frozies@users.noreply.github.com>
2021-03-16 19:27:50 -0400

    Added a signal listener for docker. added a health route.

--- petal-patch-server/app.js
+++ petal-patch-server/app.js
@@ -1,14 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan');5
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const {debug} = require("puppeteer/lib/cjs/puppeteer/common/Debug");
+var healthRouter = require('./routes/health');
+const { debug } = require("puppeteer/lib/cjs/puppeteer/common/Debug");
 
-var app = express();
 let die = false;
 
 // view engine setup
@@ -21,42 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-function shutDown() {
-  console.log('Received kill signal, shutting down gracefully');
-  console.log('Power level\'s critical... shutting down the teleporters.');
-  server.close(() => {
-    console.log('Closed out remaining connections');
-    process.exit(0);
-  });
-
-  setTimeout(() => {
-    console.error('Could not close connections in time, forcefully shutting down');
-    process.exit(1);
-  }, 10000);
-}
-
-// Health and Shutdown are for the load balancers
-app.get('/health', async (req, res) => {
-  try {
-    if (!die) {
-      res.status(200).send('Status OKAY :D');
-    } else {
-      res.status(503).send('Power level\'s critical... shutting down the teleporters.');
-    }
-
-  } catch (err) {
-    debug(err.stack);
-    res.sendStatus(500);
-  }
-});
-
-app.get('/shutdown', async () => { /*This is probably a dumb way to shut down a server... remind me to fix this later*/
-  die = true;
-  shutDown()
-});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/health', healthRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
