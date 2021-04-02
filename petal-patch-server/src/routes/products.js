cca571a15066c37f2a9467c8bd4fe7521179e8eb
Frozies <52686922+Frozies@users.noreply.github.com>
2021-04-01 22:04:49 -0400

    Cleaned input URLs for product creator. Console Logging too.

--- petal-patch-server/src/routes/products.js
+++ petal-patch-server/src/routes/products.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const puppeteer = require("puppeteer");
 const router = express.Router();
+const createDOMPurify = require('dompurify');
+const { JSDOM }  = require('jsdom');
+
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-    const [productName, productID, price, productImage] = await getTelefloraProduct();
+    const productURL = 'https://www.teleflora.com/bouquet/telefloras-bedazzling-beauty-bouquet?prodID=P_T21S100A&skuId=T21S100A&zipMin=';
+    const [productName, productID, price, productImage] = await getTelefloraProduct(productURL);
 
     res.render('products', {
         title: productName,
@@ -14,7 +21,8 @@ router.get('/', async function(req, res, next) {
     });
 });
 
-async function getTelefloraProduct() {
+async function getTelefloraProduct(url) {
+    console.log('Retrieving Teleflora Prouduct. Launching puppeteer.');
     // The headless, executablePath, and args are copy and pasted to make puppeteer work with docker.
     const browser = await puppeteer.launch({
         headless: true,
@@ -24,9 +32,19 @@ async function getTelefloraProduct() {
 
     const page = await browser.newPage();
 
-    //TODO: input data
-    await page.goto('https://www.teleflora.com/bouquet/make-a-wish?prodID=P_TEV13-6A&skuId=TEV13-6A&zipMin=');
+    //TODO: Check if URL, Check if Teleflora URL, Check if Valid Product.
+    const cleanUrl = DOMPurify.sanitize(url).toString();
+    console.log('Dirty URL: ' + url);
+    console.log('Clean URL: ' + cleanUrl);
+    if (url == cleanUrl) {
+        console.log('URLs match!');
+    } else {
+        console.log('URLs DO NOT MATCH');
+    }
 
+    await page.goto(cleanUrl);
+
+    console.log('Iterating over product');
     // Iterator for the multiple tiers of products.
     const PRICE_SELECTOR = '#sizeRadios > li:nth-child(INDEX)';
     let priceSelector = PRICE_SELECTOR.replace("INDEX", i=1);//TODO Replace the value for i for multiple products
@@ -52,8 +70,9 @@ async function getTelefloraProduct() {
         return document.getElementById("skuRadio" + i).getAttribute('data-zoom');
     }, priceSelector, i);
 
-    await browser.close();
+    console.log('Closing headless and returning product ' + productName);
 
+    await browser.close();
     return [productName, productID, price, productImage];
 }
 
