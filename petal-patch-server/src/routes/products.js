fd709b2ed048c2a73f5f98751558dc03ecbf2152
Frozies <52686922+Frozies@users.noreply.github.com>
2021-04-08 19:31:48 -0400

    Adding a product id to the /products/:productID now scrapes all of the correct information and displays it.

--- petal-patch-server/src/routes/products.js
+++ petal-patch-server/src/routes/products.js
@@ -9,34 +9,23 @@ const DOMPurify = createDOMPurify(window);
 
 
 /* GET users listing. */
-router.get('/', async function(req, res) {
-    // const productURL = 'https://www.teleflora.com/bouquet/telefloras-bedazzling-beauty-bouquet?prodID=P_T21S100A&skuId=T21S100A&zipMin=';
-    const productURL = 'http://www.teleflora.com/'; // Error: Evaluation failed: TypeError: Cannot read property 'getAttribute' of null
-    // const productURL = 'https://www.youtube.com'; // Error: Evaluation failed: TypeError: Cannot read property 'getAttribute' of null
+router.get('/:productID/', async function(req, res) {
+    const telefloraID = req.params.productID;
+    const productURL = "https://www.teleflora.com/bouquet/?prodID=" + telefloraID;
 
-    const [productName, productID, price, productImage] = await getTelefloraProduct(productURL);
-    if (productName != "Error") {
-        res.render('products', {
-            title: productName,
-            id: productID,
-            price: price,
-            image: productImage.replace("/w_800,h_1000", "/w_300,h_300")
-        });
-    } else {
-        res.render('products', {
-            title: "Error",
-            id: null,
-            price: null,
-            image: null
-        });
-    }
+    const products = await getTelefloraProduct(productURL);
+    res.render('products', {
+        results: products
+    });
+
 });
 
 async function getTelefloraProduct(dirtyUrl) {
-    const products = [];
-    let cleanUrl = sanitizeUrl(dirtyUrl);
+    let cleanUrl = new URL(dirtyUrl);
+    //TODO: Error handling for invalid URLs
+    let products = [];
 
-    console.log('Retrieving Teleflora Prouduct. Launching puppeteer.');
+    console.log('Retrieving Teleflora Product. Launching puppeteer.');
 
     // The headless, executablePath, and args are copy and pasted to make puppeteer work with docker.
     const browser = await puppeteer.launch({
@@ -50,69 +39,76 @@ async function getTelefloraProduct(dirtyUrl) {
     await page.goto(cleanUrl);
 
     console.log('Iterating over product');
+
     // Iterator for the multiple tiers of products.
-    const PRICE_SELECTOR = '#sizeRadios > li:nth-child(INDEX)';
-    let priceSelector = PRICE_SELECTOR.replace("INDEX", i=3);//TODO Replace the value for i for multiple products
+    let sizeCount = await page.$$eval('#sizeRadios > li', elements => elements.length);
 
-   try {
-       /**Retrieve Product Data*/
-           //Product Name
-       let productName = await page.evaluate((sel, i) => {
-               return document.getElementById("skuRadio" + i).getAttribute('data-alttext');
-           }, priceSelector, i);
-
-       // Product ID
-       let productID = await page.evaluate((sel, i) => {
-           return document.getElementById("skuRadio" + i).value;
-       }, priceSelector, i);
-
-       // Price of Product
-       let price = await page.evaluate((sel, i) => {
-           return document.getElementById("skuid-" + i).value;
-       }, priceSelector, i);
-
-       // Product Image
-       let productImage = await page.evaluate((sel, i) => {
-           return document.getElementById("skuRadio" + i).getAttribute('data-zoom');
-       }, priceSelector, i);
-   } catch (err) {
-       console.log("Error handling user supplied URL.");
-       console.log(err);
-       console.log("End of Error.")
-       return ["Error", "Null", "Null"];
+    for (let i=1;i<=sizeCount;i++){ // TODO: Get size range.
+        let product = await getProductData(i, page);
+        products.push(product);
     }
-
-    console.log('Closing headless and returning product ' + productName);
 
     await browser.close();
+    console.log(products);
+    return products;
+}
+
+async function getProductData(index, page) {
+    const PRICE_SELECTOR = '#sizeRadios > li:nth-child(INDEX)';
+
+    let priceSelector = PRICE_SELECTOR.replace("INDEX", index.toString());//TODO Replace the value for i for multiple products
+
+    /**Retrieve Product Data*/
+    //Product Name
+    let productName = await page.evaluate((sel, i) => {
+            return document.getElementById("skuRadio" + i).getAttribute('data-alttext');
+        }, priceSelector, index);
+
+    // Product ID
+    let productID = await page.evaluate((sel, i) => {
+        return document.getElementById("skuRadio" + i).value;
+    }, priceSelector, index);
+
+    // Price of Product
+    let price = await page.evaluate((sel, i) => {
+        return document.getElementById("skuid-" + i).value;
+    }, priceSelector, index);
+
+    // Product Image
+    let productImage = await page.evaluate((sel, i) => {
+        return document.getElementById("skuRadio" + i).getAttribute('data-zoom').replace("/w_1000,h_1000", "/w_300,h_300");
+    }, priceSelector, index);
+
+    // TODO: Fix image sizing
+    // TODO: Get product sizing (standard, deluxe, premium)
+
     console.log('Data: ' + [productName, productID, price, productImage]);
 
-
-    return [productName, productID, price, productImage];
+    return { productName: productName, productID: productID, price: price, productImage: productImage };
 }
 
-function sanitizeUrl(dirtyUrl) {
-    const cleanUrl = DOMPurify.sanitize(dirtyUrl).toString();
-
-    const getHostname = (url) => {
-        return new URL(url).hostname;
-    }
-
-    console.log('Clean URL: ' + cleanUrl);
-    if (dirtyUrl === cleanUrl) {
-        console.log('URLs match!');
-    } else {
-        console.log('URLs DO NOT MATCH');
-    }
-
-    console.log("Hostname: " + getHostname(cleanUrl));
-
-    if (getHostname(cleanUrl) == "www.teleflora.com"){
-        return cleanUrl;
-    } else {
-        return null;
-    }
-}
+// function sanitizeUrl(dirtyUrl) {
+//     const cleanUrl = DOMPurify.sanitize(dirtyUrl).toString();
+//
+//     const getHostname = (url) => {
+//         return new URL(url).hostname;
+//     }
+//
+//     console.log('Clean URL: ' + cleanUrl);
+//     if (dirtyUrl === cleanUrl) {
+//         console.log('URLs match!');
+//     } else {
+//         console.log('URLs DO NOT MATCH');
+//     }
+//
+//     console.log("Hostname: " + getHostname(cleanUrl));
+//
+//     if (getHostname(cleanUrl) == "www.teleflora.com"){
+//         return cleanUrl;
+//     } else {
+//         return null;
+//     }
+// }
 
 module.exports = router;
 
