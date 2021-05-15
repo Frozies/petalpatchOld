38781e49f51dbfebc4866442c654ad6a160db836
Frozies <52686922+Frozies@users.noreply.github.com>
2021-05-15 16:17:10 -0400

    Implemented input types for all the default product schemas

--- product-back/src/schema.js
+++ product-back/src/schema.js
@@ -14,7 +14,7 @@ const typeDefs = gql`
     }
     
     type Mutation {
-        createOrUpdateBouquet(skuid: String!, title: String!, description: String, sizes: [InputSize!]!): Bouquet
+        createOrUpdateBouquet(bouquet: inputBouquet): Bouquet
         
 #        updateBouquet(id: ID): Bouquet
 #        removeBouquet(id: ID): Bouquet
@@ -33,7 +33,6 @@ const typeDefs = gql`
     type Bouquet {
         products: [Product!]
     }
-    
     type Tags {
         size: String
         colors: Colors
@@ -62,6 +61,79 @@ const typeDefs = gql`
     }
     
     type Occasions {
+        birthday: Boolean
+        wedding: Boolean
+        christmas: Boolean
+        stpatricks: Boolean
+        easter: Boolean
+        mothers: Boolean
+        grandparents: Boolean
+        independence: Boolean
+        fathers: Boolean
+        memorial: Boolean
+        halloween: Boolean
+        thanksgiving: Boolean
+        hanukkah: Boolean
+        graduation: Boolean
+        spring: Boolean
+        anniversary: Boolean
+        backToSchool: Boolean
+        congratulation: Boolean
+        getWell: Boolean
+        imSorry: Boolean
+        justBecause: Boolean
+        romance: Boolean
+        babies: Boolean
+        religious: Boolean
+        retirement: Boolean
+        thankYou: Boolean
+        thinkingOfYou: Boolean
+        newYears: Boolean
+    }
+        
+    #### Input Types ####
+
+    input inputProduct {
+        skuid: String!
+        title: String!
+        price: Float!
+        photoURL: String
+        description: String
+        tags: [inputTags]
+    }
+
+    "Bouquet product type. Includes sizes for multiple price-points. Plan to add holiday tags and other organizers."
+    input inputBouquet {
+        products: [inputProduct!]
+    }
+    input inputTags {
+        size: String
+        colors: inputColors
+        occasions: inputOccasions
+        flowers: inputFlowers
+    }
+
+    input inputFlowers {
+        roses: Boolean
+        carnations: Boolean
+        daisys: Boolean
+        lilies: Boolean
+        stock: Boolean
+        sunflower: Boolean
+    }
+
+    input inputColors {
+        red: Boolean
+        orange: Boolean
+        yellow: Boolean
+        green: Boolean
+        blue: Boolean
+        pink: Boolean
+        purple: Boolean
+        white: Boolean
+    }
+
+    input inputOccasions {
         birthday: Boolean
         wedding: Boolean
         christmas: Boolean
