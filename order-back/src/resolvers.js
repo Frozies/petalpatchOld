85cafb41f10fed9f56876e50388ee3d3c6c4c0b1
Frozies <52686922+Frozies@users.noreply.github.com>
2021-05-22 18:24:59 -0400

    Implemented Update and Remove product

--- order-back/src/resolvers.js
+++ order-back/src/resolvers.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const orderSchema = new mongoose.Schema({
 
@@ -6,8 +7,14 @@ const orderSchema = new mongoose.Schema({
 
 const resolvers = {
     Query: {
-
-    },
+        createCustomer: () => {
+            return stripe.customers.create({
+                email: 'customer@example.com',
+            })
+                .then(customer => console.log(customer.id))
+                .catch(error => console.error(error));
+            }
+        },
 
     Mutation: {
 
--- order-back/src/schema.js
+++ order-back/src/schema.js
@@ -3,36 +3,64 @@ const {gql} = require("apollo-server");
 const typeDefs = gql`
     "Simple Query to the products backend."
     type Query {
-        retrieveOrderByID(id:ID): Order
+#        retrieveOrderByID(id:ID): Order
+        createCustomer(email: String): String
     }
     
 #    type Mutation {
 #       
 #    }
     
-    type Order {
-        id: ID,
-        customer:Customer,
-        recipient: Recipient,
-        #delivery Date
-        #products array
-        #special instructions
+#    type Order {
+#        id: ID,
+#        customer:Customer,
+#        recipient: Recipient,
+#        deliveryDate: Date
+#        products: [Product!]!
+#        totalCost: Float
+#        #special instructions
+#        
+#    }
+#    
+#    type Customer {
+#        name: String
+#        #payment
+#        #stripe information
+#        #account information?
+#    }
+#    
+#    type Recipient {
+#        name: String
+#        #Addresss
+#        #phone
+#    }
+
+    type stripeItem {
+        object: String
+        amount: Int
+        currency: String
+        description: String
+        parent: String
+        quantity: Int
+        itemType: String
+    }
+    
+    type stripeOrder {
+        id: String
+        object: String
+        amount: Int
+        amount_returned: Int
+#        application: null
+#        application_fee: null
+        charge: String
+        created: Int
+        currency: String
+#        customer: Customer
+        email: String,
+        items: [stripeItem]
+        livemode: Boolean
+#        metadata: [metadata]
         
-    }
-    
-    #extend bouquet with card message?
-    
-    type Customer {
-        name: String
-        #payment
-        #stripe information
-        #account information?
-    }
-    
-    type Recipient {
-        name: String
-        #Addresss
-        #phone
     }
 `;
 
